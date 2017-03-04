397a2f177e62ca8bf9208545d2443c575ec36d8f
Ahmet Selçuk Özyurt <asozyurt@hotmail.com>
2017-03-04 17:43:39 +0300

    Splash Activity, and carousel magazine list added

---
+++ UniversalistDergiRC.Droid/SplashActivity.cs
@@ -0,0 +1,20 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace UniversalistDergiRC.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", Label = "Universalist Dergi", Icon = "@drawable/icon", NoHistory = true, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            System.Threading.Thread.Sleep(2000); //Let's wait awhile...
+            this.StartActivity(typeof(MainActivity));
+        }
+    }
+}
+
