149e379a4e93758fe580ff8de57e7841ba691ad4
harweast <asozyurt@gmail.com>
2017-03-27 00:09:32 +0300

    icon changes

--- UniversalistDergiRC.iOS/SaveAndLoad.cs
+++ UniversalistDergiRC.iOS/SaveAndLoad.cs
@@ -3,24 +3,16 @@ using UniversalistDergiRC.DataAccess;
 using UniversalistDergiRC.Droid;
 using Xamarin.Forms;
 
-[assembly: Dependency(typeof(SaveAndLoad))]
+[assembly: Dependency(typeof(FileOperations))]
 namespace UniversalistDergiRC.Droid
 {
-    public class SaveAndLoad : IFileOperations
+    public class FileOperations : IFileOperations
     {
         public void SaveText(string filename, string text)
         {
             var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             var filePath = Path.Combine(documentsPath, filename);
             File.WriteAllText(filePath, text);
-        }
-        public string[] LoadFileLines(string filename)
-        {
-            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-            var filePath = Path.Combine(documentsPath, filename);
-            if (File.Exists(filePath))
-                return File.ReadAllLines(filePath);
-            return new string[] { string.Empty };
         }
 
         public string ReadAllText(string filename)
@@ -30,6 +22,15 @@ namespace UniversalistDergiRC.Droid
             if (File.Exists(filePath))
                 return File.ReadAllText(filePath);
             return string.Empty;
+        }
+
+        public string[] LoadFileLines(string filename)
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var filePath = Path.Combine(documentsPath, filename);
+            if (File.Exists(filePath))
+                return File.ReadAllLines(filePath);
+            return new string[] { string.Empty };
         }
     }
 }
